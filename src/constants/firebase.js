18dde5c80bb27ce90742594ef83a2100a054e5ff
techniqueFirst <techniqueFirst@github.com>
2019-05-12 14:14:21 -0400

    First commit

---
+++ src/constants/firebase.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: 'AIzaSyBxP0Bww9tQ7mCI8wb68FgJkjHwf19R6rQ',
+  authDomain: 'integro-project.firebaseapp.com',
+  databaseURL: 'https://integro-project.firebaseio.com',
+  projectId: 'integro-project',
+  storageBucket: 'integro-project.appspot.com',
+  messagingSenderId: '762822550667',
+};
